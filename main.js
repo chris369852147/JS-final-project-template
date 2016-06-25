8cba94bd1a0b12f79d3362b864b41359c82c0f45
chris369852147 <luckychris60404@gmail.com>
2016-06-25 17:52:23 +0800

    sasuke

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var sasukeImg = document.createElement("img");
+sasukeImg.src ="http://vignette4.wikia.nocookie.net/vsbattles/images/d/d0/Sasuke_Weapons_Loaded_Render_By_Skodwarde.png/revision/latest?cb=20151203062503"
+var sasuke={
+  x: 0,0 
+y: 0 ,0
+}
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 function draw(){
 ctx.drawImage(bgImg,0,0);
+ctx.drawImage( heroImg, hero.x, hero.y);
 }
 
-setTimeout( draw, 1000);
+setInterval( draw, 40);

99cfacf69adae28fa1dc0d7c0d1a504f4a535212
chris369852147 <luckychris60404@gmail.com>
2016-07-02 16:23:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var heroImg = document.createElement("img");
 heroImg.src ="images/jason.gif"
+var btnImg =document.createElement("img");
+btnImg ="images/tower-btn.png"
 var hero={
   x: 0 ,
   y: 0
@@ -13,6 +15,7 @@ var ctx = canvas.getContext("2d");
 function draw(){
 ctx.drawImage(bgImg,0,0);
 ctx.drawImage( heroImg, hero.x, hero.y);
+ctx.drawImage(btnImg,600 ,400 )
 }
 
 setInterval( draw, 40);

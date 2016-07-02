ba6d117aa98240a53ac56263962fb078739268d2
chris369852147 <luckychris60404@gmail.com>
2016-07-02 16:54:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,24 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var heroImg = document.createElement("img");
-heroImg.src ="images/jason.gif"
+heroImg.src ="images/jason.gif";
 var btnImg =document.createElement("img");
-btnImg.src ="images/tower-btn.png"
+btnImg.src ="images/tower-btn.png";
+var tower =document.createElement("img");
+towerImg.src ="images/tower.png";
 var hero={
   x: 0 ,
   y: 0
 };
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+var cursor={x:0,y:0};
 
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage( heroImg, hero.x, hero.y);
-  ctx.drawImage(btnImg,540 ,380,100,100 )
+  ctx.drawImage(btnImg,540 ,380,100,100 );
+  ctx.drawImage(towerImg,cursor.x,cursor.y);
 }
 
 setInterval( draw, 40);

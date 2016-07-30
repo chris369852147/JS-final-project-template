e2d25aee257396c24c6dd1f514915f13ad360900
chris369852147 <luckychris60404@gmail.com>
2016-07-30 18:00:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -30,12 +30,34 @@ var hero={
   x:96,
   y:448,
   speed:64,
+  pathdes:0,
   direction:{x:0,y:-1},
   move:function(){
-    this.x=this.x+this.speed*this.direction.x/FPS;
-    this.y=this.y+this.speed*this.direction.y/FPS;
+    if(iscollided(
+      enemypath[this.pathdes].x,
+      enemypath[this.pathdes].y,
+      this.x,this.y,
+      this.speed/FPS,
+      this.speed/FPS)){
+      this.x=enemypath[this.pathdes].x;
+      this.y=enemypath[this.pathdes].y;
+      this.pathdes++;
+      if(enemypath[this.pathdes].x>this.x){
+        this.direction={x:1,y:0};
+      }else if(enemypath[this.pathdes].x<this.x){
+        this.direction={x:-1,y:0};
+      }else if(enemypath[this.pathdes].y>this.y){
+        this.direction={x:0,y:1};
+      }else if(enemypath[this.pathdes].y<this.y){
+        this.direction={x:0,y:-1};
+      }
+    }else{
+      this.x=this.x+this.speed*this.direction.x/FPS;
+      this.y=this.y+this.speed*this.direction.y/FPS;
+    }
   }
 };
+
 var tower={x:0,y:0};
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");

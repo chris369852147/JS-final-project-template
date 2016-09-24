6ab4339f86019183ceb858faede5635a337410d5
chris369852147 <luckychris60404@gmail.com>
2016-09-24 17:36:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -134,6 +134,8 @@ $("#game-canvas").click(function(event){
 })
 function draw(){
   if(treehp==0){
+    ctx.font="48px Arial";
+    ctx.fillText("Game over",64,64)
   clearInterval(setInterval);
 }else{
   if(clock%80==0){

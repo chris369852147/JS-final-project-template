bc5e13457d3a894f81f799a0eeb5771382b5761e
chris kuo <17724926+chris369852147@users.noreply.github.com>
2019-08-20 16:53:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ function iscollided(pointX,pointY,targetX,targetY,targetW,targetH){
 }
 
 function Hero(){
-  this.hp=1+clock;
+  this.hp=1+clock*0.2;
   this.x=96;
   this.y=448;
   this.speed=64;
